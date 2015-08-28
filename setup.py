cc9f39c8e80a662366c7e1e12d40a325c6f72740
Asher Bender <a.bender.dev@gmail.com>
2015-08-29 01:42:55 +1000

    setup: Setup.py script added to project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pwdgen',
+    version='1.0',
+    author='Asher Bender',
+    author_email='a.bender.dev@gmail.com',
+    description=('Generate random passwords.'),
+    scripts=['pwdgen'],
+)
